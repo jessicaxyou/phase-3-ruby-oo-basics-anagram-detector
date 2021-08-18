702296cc67df70c6bbc5f171c3cbc6c2016e9a43
jessica you <jessicaxytan@gmail.com>
2021-08-18 15:20:00 -0400

    commit tree a04a1dae89c1cd74afe3344e363a289aa55b99eb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match(array)
+        new_array = []
+        array.map do |item|
+            if item.chars.sort == @word.chars.sort
+                new_array << item
+            end
+        end
+        new_array
+    end
+
+end
